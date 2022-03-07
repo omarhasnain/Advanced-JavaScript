b5601f25ab29e7cebbba35b50a1bd1a54fb4d3ff
TeddyBear <e201023@ugrad.iiuc.ac.bd>
2022-03-08 01:13:56 +0500

    final commit

---
+++ null-undefined.js
@@ -0,0 +1,22 @@
+let Zebra;
+console.log(Zebra);
+
+function add(num1 , num2){
+    console.log(num1+num2);
+    return 
+}
+function add(num1,num2){
+    console.log(num1,num2);
+}
+
+
+const premik={name:"smart", phone:"01898"};
+console.log(premik.gf);
+
+
+let fun = undefined;
+console.log(fun);
+
+
+let ages= [2,5,9];
+console.log(ages[11]);
